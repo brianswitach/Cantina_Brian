a429ccdafac1027721db146b83aa89936bcd4f9b
Brian Switach <brianswitach@air-de-brian.lan>
2024-12-05 14:02:47 -0300

    a

---
+++ js/validation.js
@@ -0,0 +1,12 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if (!name || !email || !message) {
+        alert("Todos los campos son obligatorios.");
+        event.preventDefault();
+    } else {
+        alert("Formulario enviado correctamente.");
+    }
+});
